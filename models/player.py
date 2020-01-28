dc1e231305d788b8982e35b1417dc0a8cd3513f4
Daniel Villaca <danielvillacasilva@gmail.com>
2020-01-28 19:41:23 -0300

    Start me up

---
+++ models/player.py
@@ -0,0 +1,16 @@
+class Player():
+
+    id = ""
+    full_name = ""
+    first_name = ""
+    last_name = ""
+    is_active = False
+
+    #ball usage
+    #ball possegion%
+    #rebound%
+    #assist%
+    #heatmap data?
+    #tpa?
+
+
---
+++ models/team.py
@@ -0,0 +1,16 @@
+class Team():
+
+    id = ""
+    full_name = ""
+    abbreviation = ""
+    nickname = ""
+    city = ""
+    state = ""
+    year_founded = ""
+
+    #pace
+    #off rating
+    #def rating
+    #avg speed leaguedashptstats
+    #fastbreak data?
+    #distance leaguedashptstats
